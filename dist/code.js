5e75c8d27339f85c4a0efc65bbe9766078f1d5ac
Sai Perchard <perchard@fb.com>
2023-01-17 14:58:46 -0800

    initial commit

---
+++ dist/code.js
@@ -0,0 +1,2 @@
+"use strict";
+figma.showUI(`<script>window.location.href = "http://localhost:3000"</script>`, { width: 300, height: 360 });
---
+++ plugin/code.ts
@@ -0,0 +1,4 @@
+figma.showUI(
+ `<script>window.location.href = "http://localhost:3000"</script>`,
+ { width: 300, height: 360 }
+)
